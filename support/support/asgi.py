a480e8dfaa1d03bafe6a2fedbc1c0b4b7d230bd4
mirlondev <mirlon.networking@gmail.com>
2025-11-14 10:07:27 +0100

    manage python and settings

--- support/support/asgi.py
+++ support/support/asgi.py
@@ -2,7 +2,7 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "support.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
 django.setup()  # ⚠️ Initialise les apps
 
 from django.core.asgi import get_asgi_application
