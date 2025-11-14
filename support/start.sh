37237049f9d9ce0ca70258da730db2dd8e874c07
mirlondev <mirlon.networking@gmail.com>
2025-11-14 09:45:40 +0100

    Fix: Add execute permissions to start scripts

--- support/start.sh
+++ support/start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# Donner les permissions d'exécution au script dans support/
+chmod +x support/start.sh
+
+# Exécuter le script
+./support/start.sh
+
 python manage.py collectstatic --noinput
 gunicorn support.wsgi:application --bind 0.0.0.0:$PORT
